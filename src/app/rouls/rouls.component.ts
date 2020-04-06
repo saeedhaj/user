04adce8ac18e6bd8cd613df70ccca541b8bda416
saeedhaj <saeed.hajiaghazadeh@gmai.com>
2020-04-07 01:53:19 +0330

    change to role

--- src/app/rouls/rouls.component.ts
+++ src/app/rouls/rouls.component.ts
@@ -8,11 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class RoulsComponent implements OnInit {
   name: string;
   desc: string;
+  roles: any[] = [
+    {
+      name: 'ali',
+      desc: 'jdfhkdfk',
+
+    },
+    {
+      name: 'ali',
+      desc: 'jdfhkdfk',
+
+    },
+    {
+      name: 'ali',
+      desc: 'jdfhkdfk',
+    },
+
+  ]
+  isAddingRoles = false;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-  clickMe() {
-console.log(`${this.name} ${this.desc}`)
+
+  onSaveClick() {
+    const role = {
+      name: this.name,
+      desc: this.desc
+    };
+    this.roles.push(role);
+    this.isAddingRoles = false;
+    this.resetForm()
+    // console.log(`${this.name} ${this.desc}`)
+  }
+
+
+  onCreateNewRolesClick() {
+    this.isAddingRoles = true;
+  }
+
+  onCreateNewRolesCancelClick() {
+    this.isAddingRoles = false;
+  }
+
+  resetForm() {
+    this.name = '';
+    this.desc = '';
+
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,75 +12,75 @@ export class UserComponent implements OnInit {
   mobail: number;
   post: number;
   addres: string;
-  // users: any[] = [
-  //   {
-  //     firstName: 'Saeed',
-  //     lastName: 'Hajiaghazadeh',
-  //     phone: '021545454',
-  //     mobile: '0912545454',
-  //     post: '654654654',
-  //     address: 'Tehran , ....'
-  //   },
-  //   {
-  //     firstName: 'Mohammad',
-  //     lastName: 'Haji',
-  //     phone: '021545454',
-  //     mobile: '0937545454',
-  //     post: '564654654',
-  //     address: 'Tehran ,Gharchak , ...'
-  //   }, {
-  //     firstName: 'Vahid',
-  //     lastName: 'Haji',
-  //     phone: '0221545454',
-  //     mobile: '09375454',
-  //     post: '5454',
-  //     address: 'Gharchak sdfds'
-  //   }, {
-  //     firstName: 'Soheil',
-  //     lastName: 'Hajiagha',
-  //     phone: '022154545454',
-  //     mobile: '0937545422',
-  //     post: '54544444',
-  //     address: 'Varamin, ....'
-  //   },
-  // ]
-  // isAddingUser = false;
+  users: any[] = [
+    {
+      firstName: 'Saeed',
+      lastName: 'Hajiaghazadeh',
+      phone: '021545454',
+      mobile: '0912545454',
+      post: '654654654',
+      address: 'Tehran , ....'
+    },
+    {
+      firstName: 'Mohammad',
+      lastName: 'Haji',
+      phone: '021545454',
+      mobile: '0937545454',
+      post: '564654654',
+      address: 'Tehran ,Gharchak , ...'
+    }, {
+      firstName: 'Vahid',
+      lastName: 'Haji',
+      phone: '0221545454',
+      mobile: '09375454',
+      post: '5454',
+      address: 'Gharchak sdfds'
+    }, {
+      firstName: 'Soheil',
+      lastName: 'Hajiagha',
+      phone: '022154545454',
+      mobile: '0937545422',
+      post: '54544444',
+      address: 'Varamin, ....'
+    },
+  ]
+  isAddingUser = false;
 
-  // constructor() { }
+  constructor() { }
 
-  // ngOnInit(): void {
-  // }
+  ngOnInit(): void {
+  }
 
-  // onSaveClick() {
-  //   const newUser = {
-  //     firstName: this.fname,
-  //     lastName: this.lname,
-  //     phone: this.phone,
-  //     mobile: this.mobail,
-  //     post: this.post,
-  //     address: this.addres
-  //   };
-  //   this.users.push(newUser);
-  //   // console.log(`${this.fname} ${this.lname} ${this.phone} ${this.mobail} ${this.post} ${this.addres}`);
-  //   this.isAddingUser = false;
-  //   this.resetForm()
-  // }
+  onSaveClick() {
+    const newUser = {
+      firstName: this.fname,
+      lastName: this.lname,
+      phone: this.phone,
+      mobile: this.mobail,
+      post: this.post,
+      address: this.addres
+    };
+    this.users.push(newUser);
+    // console.log(`${this.fname} ${this.lname} ${this.phone} ${this.mobail} ${this.post} ${this.addres}`);
+    this.isAddingUser = false;
+    this.resetForm()
+  }
 
-  // onCreateNewUserClick() {
-  //   this.isAddingUser = true;
-  // }
+  onCreateNewUserClick() {
+    this.isAddingUser = true;
+  }
 
-  // onCreateNewUserCancelClick() {
-  //   this.isAddingUser = false;
-  // }
+  onCreateNewUserCancelClick() {
+    this.isAddingUser = false;
+  }
 
-  // resetForm() {
-  //   this.fname = '';
-  //   this.lname = '';
-  //   this.phone = undefined;
-  //   this.mobail = undefined;
-  //   this.post = undefined;
-  //   this.addres = undefined;
-  // }
+  resetForm() {
+    this.fname = '';
+    this.lname = '';
+    this.phone = undefined;
+    this.mobail = undefined;
+    this.post = undefined;
+    this.addres = undefined;
+  }
 
 }
