db2c269f941075136bec7ac190a4b5bce9635734
saeedhaj <saeed.hajiaghazadeh@gmai.com>
2020-04-06 15:51:30 +0330

    make to module add-user

--- src/app/user-detail/user-detail-routing.module.ts
+++ src/app/user-detail/user-detail-routing.module.ts
@@ -6,7 +6,13 @@ import { UserDetailComponent } from './user-detail.component';
 const routes: Routes = [
   {
     path:'',
-    component: UserDetailComponent
+    component: UserDetailComponent,
+    children:[
+      {
+        path:'add-user',
+        loadChildren:() =>import("../add-user/add-user-routing.module").then(m=>m.AddUserRoutingModule)
+      }
+    ]
   }
 ];
 
