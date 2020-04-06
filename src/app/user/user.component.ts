84e7db2ea785974459e99b8c1a14e4ed82aa49eb
saeedhaj <saeed.hajiaghazadeh@gmai.com>
2020-04-06 13:58:23 +0330

    get user form values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  fname: string;
+  lname: string;
+  phone: number;
+  mobail: number;
+  post: number;
+  addres:string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  save(){
+    console.log(`${this.fname} ${this.lname} ${this.phone} ${this.mobail} ${this.post} ${this.addres}`)
+  }
 }
