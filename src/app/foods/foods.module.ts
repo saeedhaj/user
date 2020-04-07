ce66b08a456d8c6939ad29a90f6e81adb6c0b15f
saeedhaj <saeed.hajiaghazadeh@gmai.com>
2020-04-07 08:53:45 +0330

    add new module foods

---
+++ src/app/foods/foods.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FoodsRoutingModule } from './foods-routing.module';
+import { FoodsComponent } from './foods.component';
+
+
+@NgModule({
+  declarations: [FoodsComponent],
+  imports: [
+    CommonModule,
+    FoodsRoutingModule
+  ]
+})
+export class FoodsModule { }
